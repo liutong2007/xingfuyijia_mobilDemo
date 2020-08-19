2e44ac130776972a983a92539f7aca9cc867fa56
liutongtong <liutongtong@zichan360.com>
2020-08-19 10:05:01 +0800

    1

--- demoMobile/static/js/page.js
+++ demoMobile/static/js/page.js
@@ -1,5 +1,10 @@
 'use strict';
-
+function getQueryString(name) { 
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+    var r = window.location.search.substr(1).match(reg); 
+    if (r != null) return unescape(r[2]); 
+    return null; 
+}
 /*rem处理*/
 var AfedFn = {};
 var myScroll;
@@ -980,5 +985,21 @@ function loadFn(){
 
 }
 
-
+function browserRedirect() {
+　　var sUserAgent = navigator.userAgent.toLowerCase();
+　　var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+　　var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+　　var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+　　var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+　　var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+　　var bIsAndroid = sUserAgent.match(/android/i) == "android";
+　　var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+　　var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+　　if ((bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+　　　　window.location.href="http://www.91hesuan.com/yijiaDemo";
+　　} else {
+　　　　window.location.href="http://www.91hesuan.com/yijiaDemoPc";
+　　}　　　　
+}
+// browserRedirect();
 
